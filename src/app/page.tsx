0d389193a6738cd19ef07ab64d0e79aa5caec39a
Andredy Guerra <andredyguerra00@gmail.com>
2025-01-29 15:52:26 -0500

    implementacion de grafica barChart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 import CreateForm from "./ui/create-form";
 import Table from "./ui/table";
 import { getLoanData } from "./lib/action";
+import MyChart from "./ui/bar-chart";
 
 export default async function Home() {
-
-  const initialData = await getLoanData()
+  const initialData = await getLoanData();
 
   return (
     <div className="bg-gris-principal w-3/4 p-3 rounded-md">
       <div className="outline-dashed-gris h-full p-5">
         <CreateForm />
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+          <MyChart />
+        </div>
         <Table loanData={initialData} />
       </div>
     </div>
